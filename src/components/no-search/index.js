10d5d5954d64cd813c29890047806a82af00ca9c
Jonatan Marques <102691032+jonatanmarss@users.noreply.github.com>
2022-09-29 16:06:06 -0300

    Initial commit

---
+++ src/components/no-search/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./styled";
+
+const NoSearch = () => {
+  return (
+    <S.Wrapper>
+      <h1>
+        Nenhum usuario pesquisado, digite um nome de usuario do GitHub acima
+      </h1>
+    </S.Wrapper>
+  );
+};
+
+export default NoSearch;
